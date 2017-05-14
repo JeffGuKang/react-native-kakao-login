fcf251dc3d79b2f8c3c5ae15cb4343e9caef3029
Jeff Gu Kang <jeffgukang@gmail.com>
2017-05-14 18:54:23 +0900

    Update `login` and `userInfo` in ReactNativeKakao.m

--- ios/ReactNativeKakao/ReactNativeKakao.m
+++ ios/ReactNativeKakao/ReactNativeKakao.m
@@ -28,6 +28,10 @@ RCT_EXPORT_MODULE();
 						@"KOAuthTypeAccount" : @(KOAuthTypeAccount) };
 };
 
+/**
+ Login or Signup
+ @param authTypes array consists in KOAuthType.
+ */
 RCT_REMAP_METHOD(login,
 				 authTypes: (NSArray* )authTypes
 				 resolver:(RCTPromiseResolveBlock)resolve
@@ -36,29 +40,74 @@ RCT_REMAP_METHOD(login,
 	dispatch_async(dispatch_get_main_queue(), ^{
 		[[KOSession sharedSession] close];
 		[[KOSession sharedSession] openWithCompletionHandler:^(NSError *error) {
+			NSLog(@"MYLOG: openWithCompletionHandler");
+
 			if(error) {
-				reject(@"RNKakao", @"login faild", error);
+				NSLog(@"Error: %@", error.description);
+				NSLog(@"%@", error.description);
+
+				reject(@"RNKakao", @"login error", error);
 				return;
 			}
 
 			if ([[KOSession sharedSession] isOpen]) {
-				[self loginResolve:resolve rejecter:reject];
+				NSLog(@"sharedSession is open");
+
+				[self userInfoRequestResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject];
+				return;
 			} else {
 				reject(@"RNKakao", @"login canceled", nil);
+				return;
 			}
 		} authParams:nil authTypes:(authTypes != nil) ? authTypes : @[@(KOAuthTypeTalk), @(KOAuthTypeStory), @(KOAuthTypeAccount)]];
 	});
 }
 
-- (void)loginResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+/**
+ Get userInfo
+ */
+RCT_REMAP_METHOD(userInfo,
+				 resolver:(RCTPromiseResolveBlock)resolve
+				 rejecter:(RCTPromiseRejectBlock)reject)
+{
+	[self userInfoRequestResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject];
+}
+
+- (void) userInfoRequestResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
 	[KOSessionTask meTaskWithCompletionHandler:^(KOUser* result, NSError *error) {
 		if (result) {
 			// success
-			NSDictionary *response = @{@"user": result};
-			resolve(response);
+
+			NSNumber *id = result.ID;
+			NSString *nickName = [result propertyForKey:KOUserNicknamePropertyKey];
+			NSString *email = [result propertyForKey:KOUserEmailPropertyKey];
+			NSString *profileImage = [result propertyForKey:KOUserProfileImagePropertyKey];
+			NSString *profileImageThumnail = [result propertyForKey:KOUserThumbnailImagePropertyKey];
+
+			NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+			[userInfo setValue:id forKey:@"id"];
+			[userInfo setValue:[KOSession sharedSession].accessToken forKey:@"accessToken"];
+
+			if (nickName != nil) {
+				[userInfo setValue:nickName forKey:@"nickName"];
+			}
+
+			if (email != nil) {
+				[userInfo setValue:email forKey:@"email"];
+			}
+
+			if (profileImage != nil) {
+				[userInfo setValue:profileImage forKey:@"profileImage"];
+			}
+
+			if (profileImageThumnail != nil) {
+				[userInfo setValue:profileImageThumnail forKey:@"profileImageThumnail"];
+			}
+
+			resolve(userInfo);
 		} else {
 			// failed
-			reject(@"RNKakao", @"login error", error);
+			reject(@"RNKakao", @"userInfo error", error);
 		}
 	}];
 }
