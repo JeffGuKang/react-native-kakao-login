de30c98384b51a3c4630797539add18a04fa9f33
Jeff Gu Kang <jeffgukang@gmail.com>
2019-08-11 14:59:07 +0900

    Change folder name

--- Examples/ReactNativeKakaoExample/appcenter-post-clone.sh
+++ Examples/ReactNativeKakaoExample/appcenter-post-clone.sh
@@ -10,13 +10,6 @@
 # mkdir ~/Documents  
 # ln -s $APPCENTER_SOURCE_DIRECTORY/ios/FacebookSDK ~/Documents/FacebookSDK
 
-#!/usr/bin/env bash
-
-# echo "Uninstalling all CocoaPods versions"
-# sudo gem uninstall cocoapods --all --executables
-
-# echo "Install cocoapod 1.7.4"
-# sudo gem install cocoapods -v 1.7.4
-
-# echo "Pod install"
-# cd ios && pod install
+echo "APPCENTER_SOURCE_DIRECTORY: " + $APPCENTER_SOURCE_DIRECTORY
+echo "Link rn-kakao-login folder"
+ln -s $APPCENTER_SOURCE_DIRECTORY/rn-kakao-login ../../rn-kakao-login
