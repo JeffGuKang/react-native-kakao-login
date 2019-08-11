848f2349d16b0c30e27b9903bb3c0799e0ded00d
Jeff Gu Kang <jeffgukang@gmail.com>
2019-08-11 14:37:50 +0900

    Appcenter

---
+++ Examples/ReactNativeKakaoExample/appcenter-post-clone.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+# git clone https://github.com/example/SomeProject
+
+# Example: Install App Center CLI
+# npm install -g appcenter-cli
+
+# Creating virtual symbolic link for FB SDK for buddybuild
+# mkdir ~/Documents  
+# ln -s $APPCENTER_SOURCE_DIRECTORY/ios/FacebookSDK ~/Documents/FacebookSDK
+
+#!/usr/bin/env bash
+
+# echo "Uninstalling all CocoaPods versions"
+# sudo gem uninstall cocoapods --all --executables
+
+# echo "Install cocoapod 1.7.4"
+# sudo gem install cocoapods -v 1.7.4
+
+cd $APPCENTER_SOURCE_DIRECTORY && pod install
